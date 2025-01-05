431878aa2f744f2a26f330254be0c80f1b884577
Muhammad Kashif Irshad <kashif.ir@gmail.com>
2025-01-05 14:47:43 +0500

    commit
    commit

--- pmc_be/wsgi.py
+++ pmc_be/wsgi.py
@@ -6,32 +6,25 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-from pmc_be import settings
 
-"""
-WSGI config for pmc_be project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 
 import os
 import sys
 
-sys.path.append('/var/www/vhosts/pmc/pmc_be')
-sys.path.append('/var/www/vhosts/pmc/myenv2/lib/python3.12/site-packages')
+# Add your project and virtualenv paths to sys.path
+sys.path.append('/var/www/vhosts/staging/pmc_be')
+sys.path.append('/var/www/vhosts/staging/myenv2/lib/python3.12/site-packages')
 
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-from django.core.wsgi import get_wsgi_application
-
-#
-# # add the virtualenv site-packages path to the sys.path
-
+# Set the environment variable for Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pmc_be.settings")
 
-if settings.DEBUG:
+# Import WSGI application
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+# Use StaticFilesHandler in production if serving static files through WSGI
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'pmc_be.settings':
     application = StaticFilesHandler(get_wsgi_application())
 else:
     application = get_wsgi_application()
+
